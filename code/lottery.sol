58a3c1da78f29fb613c8b8eb55ec59784d8716d2
Deryck97 <38873110+Deryck97@users.noreply.github.com>
2018-05-15 13:43:34 -0700

    Update lottery.sol

--- code/lottery.sol
+++ code/lottery.sol
@@ -1,36 +1,42 @@
 pragma solidity ^0.4.22;
 
-contract Lottery{
+contract Lottery {
 
-  address[] public players;
+    //Create array to store player addresses
+    address[] public players;
   
-  address public manager;
-  
-  constructor() Lottery() public {
-  
-    manager = msg.sender; 
+    //Create manager account variable
+    address public manager;
+
+    constructor() Lottery() public {
+        //Assign contract creator as the manager
+        manager = msg.sender; 
     }
+
+    function enter() public payable {
+        //Require players to send 1 ether when they enter the lottery
+        require(msg.value >= 1 ether);
     
-  function enter() public payable {
-  
-    require(msg.value >= 1 ether);
-    
-    players.push(msg.sender); 
+        //Add new player to the players array
+        players.push(msg.sender); 
     }
-    
-  function random() private view returns (uint) {
-  
-    return uint(keccak256(block.difficulty, now, players));
+
+    function random() private view returns (uint) {
+        //Return a psuedo random number uint 
+        return uint(keccak256(block.difficulty, now, players)); 
     }
-    
-  function pickWinner() public {
-  
-    require(msg.sender == manager);
-    
-    uint index = random() % players.length;
-    
-    players[index].transfer(this.balance);
-    
-    players = new address[](0); 
+
+    function pickWinner() public {
+        //Only the manager can call this function
+        require(msg.sender == manager);
+
+        //Assign winning index to a uint
+        uint index = random() % players.length;
+
+        //Transfer the lottery balance to winner
+        players[index].transfer(this.balance);
+        
+        //Clear players array 
+        players = new address[](0); 
     }
 }
